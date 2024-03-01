5c7f631be510fa81b955c503398ce51646d1f723
SvitlanaParyiska <125573969+SvitlanaParyiska@users.noreply.github.com>
2024-03-01 21:51:54 +0000

    Deploying to gh-pages from @ SvitlanaParyiska/medicine-delivery-app-front@8fdaa0b5ff42fcfdc8177aa5298a1aab171748c0 🚀

--- static/js/176.b362a236.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkmedicine_delivery_app_front=self.webpackChunkmedicine_delivery_app_front||[]).push([[176],{176:(e,n,i)=>{i.r(n),i.d(n,{default:()=>c});var r=i(579);const c=function(){return(0,r.jsx)("div",{children:"Home"})}}}]);
-//# sourceMappingURL=176.b362a236.chunk.js.map
---
+++ static/js/775.cfb31542.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkmedicine_delivery_app_front=self.webpackChunkmedicine_delivery_app_front||[]).push([[775],{775:(e,n,i)=>{i.r(n),i.d(n,{default:()=>c});var r=i(579);const c=function(){return(0,r.jsx)("main",{children:(0,r.jsx)("div",{children:"Shopping Cart"})})}}}]);
+//# sourceMappingURL=775.cfb31542.chunk.js.map

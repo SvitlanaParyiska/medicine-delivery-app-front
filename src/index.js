0c10ff36876818f7c9585f28fcf72fe1cb2b6590
SvitlanaParyiska <paryiskasvitlana@gmail.com>
2024-03-01 16:58:12 +0200

    fixed

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import App from "./components/App";
-import Loader from "./components/Loader";
+// import Loader from "./components/Loader";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
